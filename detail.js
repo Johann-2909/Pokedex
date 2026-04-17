f375a3fc078abe52ad15dc307b9f23ce94d79af4
Johann Balzereit <johannbalzereit99@gmail.com>
2026-04-17 11:14:47 +0200

    commit

--- detail.js
+++ detail.js
@@ -3,13 +3,7 @@ const params = new URLSearchParams(window.location.search)
 const name = params.get("pokemon")
 
 
-async function detailsPokemon() {
-    const details = await P.getPokemonByName(name)
-    const pokemonName = details.name
-    const image = details.sprites.other["official-artwork"].front_default
-    const type = details.types[0].type.name
-    const weight = details.weight
-    const height = details.height
+function getDetailsPokemon(pokemonName, image, type, weight, height) {
     const card = document.createElement('div');
     card.className = 'pokemon-card';
     card.innerHTML = `
@@ -21,10 +15,43 @@ async function detailsPokemon() {
         <p>Height: ${height / 10} m</p>
       </div>
     `
-    card.dataset.name = name
+    card.dataset.pokemonName = name
     card.dataset.type = type
-    document.getElementById("details").appendChild(card)
+    return card;
 }
 
+async function loadPokemonDetails() {
+    const details = await P.getPokemonByName(name)
+    const pokemonName = details.name
+    const image = details.sprites.other["official-artwork"].front_default
+    const type = details.types[0].type.name
+    const weight = details.weight
+    const height = details.height
+    const stats = details.stats
+    const card = getDetailsPokemon(name, image, type, weight, height);
+    const statsCard = statsDetails(stats);
+    const container = document.getElementById("details");
+    container.appendChild(card);
+    container.appendChild(statsCard);
+}
 
-detailsPokemon()
+function statsDetails(stats) {
+    const statCard = document.createElement('div');
+    statCard.className = 'stat-card';
+
+    for (const stat of stats) {
+        const statItem = document.createElement('div');
+        statItem.className = 'stat-details';
+
+        statItem.innerHTML = `
+            <h3>${stat.stat.name}</h3>
+            <p>Base Stat: ${stat.base_stat}</p>
+        `;
+
+        statCard.appendChild(statItem);
+    }
+
+    return statCard;
+}
+
+loadPokemonDetails()
