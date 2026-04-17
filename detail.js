d7e353c2927be8c927d4b8a9849f99abeb670a2f
Johann Balzereit <johannbalzereit99@gmail.com>
2026-04-17 10:21:13 +0200

    commit

--- detail.js
+++ detail.js
@@ -6,12 +6,12 @@ const name = params.get("pokemon")
 async function detailsPokemon() {
     const details = await P.getPokemonByName(name)
     const pokemonName = details.name
-    const image = details.sprites.front_default
+    const image = details.sprites.other["official-artwork"].front_default
     const type = details.types[0].type.name
     const weight = details.weight
     const height = details.height
     const card = document.createElement('div');
-    card.className = 'pokemon-card-details';
+    card.className = 'pokemon-card';
     card.innerHTML = `
       <div class="card-details">
         <img src="${image}">
